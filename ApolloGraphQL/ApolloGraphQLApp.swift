a4ae9ab0e30705be83a2df4adf7ec3b900dfd25a
Shuhei Kuroda <shu.0915@gmail.com>
2022-04-19 02:18:29 +0900

    SampleのGraphQLファイルとAPI.swiftファイルの生成まで

---
+++ ApolloGraphQL/ApolloGraphQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApolloGraphQLApp.swift
+//  ApolloGraphQL
+//
+//  Created by Shuhei Kuroda on 2022/04/08.
+//
+
+import SwiftUI
+
+@main
+struct ApolloGraphQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApolloGraphQL/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ApolloGraphQL
+//
+//  Created by Shuhei Kuroda on 2022/04/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
